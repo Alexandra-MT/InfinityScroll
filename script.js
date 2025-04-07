ae07e65498e0d1c42f7e85c028a834b7d17d9454
Alexandra Madalina Tutica <147504323+Alexandra-MT@users.noreply.github.com>
2025-04-07 16:46:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Unsplash API
 const count = 10;
-const apiKey = 'Q22uBfjBn5jv8cfuT4F8tjgNwvyOyJ_VVTDqKyHdrdM';
+const apiKey = '';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 //Get fotos Unsplash Api
